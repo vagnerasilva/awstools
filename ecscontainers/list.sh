49b9a5fc1d2ddc260a0248c9c92cc5e8d5547f5a
vagnerasilva <vagnerasilva>
2021-08-17 19:58:00 -0300

    novo

---
+++ ecscontainers/list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "#####   #####"
+echo "#####  Scrip de interacao com ECS AWS #####"
+echo "#####   #####"
+
+##### em caso de necessidade use chmod +x file
+echo "Seu nome de usuário é:"
+whoami
+echo "#####   #####"
+echo "O script está executando do diretório:"
+pwd
+
+
+echo "#####  Checando status  #####"
